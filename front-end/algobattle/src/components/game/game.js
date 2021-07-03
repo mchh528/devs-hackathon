bd2e95ca17b0f27f6c6ea8a75bebc79ec6c279bd
JD Briones <jdbriones22@gmail.com>
2021-07-03 17:39:52 +1200

    finalized game page

--- front-end/algobattle/src/components/game/game.js
+++ front-end/algobattle/src/components/game/game.js
@@ -2,28 +2,56 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Ide from "./ide";
 import Prompt from "./prompt";
 import classes from "./game.module.css";
+import ReadIde from "./readIde";
 
 function Game() {
   return (
     <div>
-      <Grid container spacing={1} className={classes.backgroundColor}>
-        <Grid item xs>
-          <Prompt
-            title="Title"
-            description="Sed ut perspiciatis unde omnis iste natus error sit 
-            voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae 
-            vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
-            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem 
-            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam 
-            eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-             nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
-             velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-          />
+      <Grid
+        container
+        spacing={1}
+        className={classes.backgroundColor}
+        direction="row"
+      >
+        <Grid container direction="column" xs={4}>
+          <Grid item xs>
+            <Prompt
+              title="Simple Array Sum"
+              description={
+                <p>
+                  Given an array of integers, find the sum of its elements.
+                  <p>
+                    For example, if the array ar = [1, 2, 3], 1 + 2 + 3 = 6, so
+                    return 6
+                    <p>
+                      <strong>Input Format</strong>
+                      <p>
+                        The first line contains an integer, 'n', denoting the
+                        size of the array. The second line contains 'n'
+                        space-separated integers representing the array's
+                        elements.
+                      </p>
+                      <strong>Output Format</strong>
+                      <p>
+                        Print the sum of the array's elements as a single
+                        integer.
+                      </p>
+                      <strong>Sample Input</strong>
+                      <p>6</p>
+                      <p>1 2 3 4 10 11</p>
+                      <strong>Sample Output</strong>
+                      <p>31</p>
+                    </p>
+                  </p>
+                </p>
+              }
+            />
+          </Grid>
+          <Grid item xs>
+            <ReadIde />
+          </Grid>
         </Grid>
-        <Grid item xs>
+        <Grid item xs={8} direction="column">
           <Ide />
         </Grid>
       </Grid>
